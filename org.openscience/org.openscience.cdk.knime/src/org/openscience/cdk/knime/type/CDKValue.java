75bdcc6d59de615c0df51a341337cf6b5083ce23
thor <thor@cd80d6f6-f959-11df-8f91-f7e70cbaf77b>
2015-09-04 12:11:44 +0000

    fixed issue with renderer registration
    git-svn-id: https://community.knime.org/svn/nodes4knime/trunk@8386 cd80d6f6-f959-11df-8f91-f7e70cbaf77b

--- org.openscience/org.openscience.cdk.knime/src/org/openscience/cdk/knime/type/CDKValue.java
+++ org.openscience/org.openscience.cdk.knime/src/org/openscience/cdk/knime/type/CDKValue.java
@@ -1,17 +1,17 @@
 /*
  * Copyright (C) 2003 - 2013 University of Konstanz, Germany and KNIME GmbH, Konstanz, Germany Website:
  * http://www.knime.org; Email: contact@knime.org
- * 
+ *
  * This file is part of the KNIME CDK plugin.
- * 
+ *
  * The KNIME CDK plugin is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser
  * General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your
  * option) any later version.
- * 
+ *
  * The KNIME CDK plugin is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
  * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
  * for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License along with the plugin. If not, see
  * <http://www.gnu.org/licenses/>.
  */
@@ -19,44 +19,41 @@ package org.openscience.cdk.knime.type;
 
 import javax.swing.Icon;
 
-import org.knime.core.data.DataColumnSpec;
 import org.knime.core.data.DataValue;
 import org.knime.core.data.DataValueComparator;
-import org.knime.core.data.renderer.DataValueRendererFamily;
-import org.knime.core.data.renderer.DefaultDataValueRendererFamily;
-import org.knime.core.data.renderer.MultiLineStringValueRenderer;
+import org.knime.core.data.ExtensibleUtilityFactory;
 import org.openscience.cdk.interfaces.IAtomContainer;
 
 /**
  * DataValue for a Molecule.
- * 
+ *
  * @author Bernd Wiswedel, University of Konstanz
  */
 public interface CDKValue extends DataValue {
 
 	/**
 	 * Meta information to this value type.
-	 * 
+	 *
 	 * @see DataValue#UTILITY
 	 */
 	public static final UtilityFactory UTILITY = new CDKUtilityFactory();
 
 	/**
 	 * Returns the Smiles string of the molecule.
-	 * 
+	 *
 	 * @return a String value
 	 */
 	String getSmilesValue();
 
 	/**
 	 * Get the CDK atom container.
-	 * 
+	 *
 	 * @return the corresponding atom container
 	 */
 	IAtomContainer getAtomContainer();
 
 	/** Implementations of the meta information of this value class. */
-	public static class CDKUtilityFactory extends UtilityFactory {
+	public static class CDKUtilityFactory extends ExtensibleUtilityFactory {
 
 		/** Singleton icon to be used to display this cell type. */
 		private static final Icon ICON = loadIcon(CDKValue.class, "/cdk.png");
@@ -76,7 +73,7 @@ public interface CDKValue extends DataValue {
 
 		/** Only subclasses are allowed to instantiate this class. */
 		protected CDKUtilityFactory() {
-
+			super(CDKValue.class);
 		}
 
 		/**
@@ -95,13 +92,9 @@ public interface CDKValue extends DataValue {
 			return COMPARATOR;
 		}
 
-		/**
-		 * {@inheritDoc}
-		 */
 		@Override
-		protected DataValueRendererFamily getRendererFamily(final DataColumnSpec spec) {
-			return new DefaultDataValueRendererFamily(new CDKValueRenderer(),
-					new MultiLineStringValueRenderer("String"));
+		public String getName() {
+			return "CDK Molecule";
 		}
 	}
 }
